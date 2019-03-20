55185afbcdaeeb4ebf3af5570a68fdfbea45a034
Sim Greenbaum <gbs4ever2@gmail.com>
2019-03-20 02:33:30 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,6 +11,7 @@ class StudentsController < ApplicationController
 
     @student = Student.find(params[:id])
     @student.active = !@student.active
+    @student.save
   #render 'views/students/show.html.erb'
   end
 
